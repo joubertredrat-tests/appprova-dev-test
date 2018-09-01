560ca369d4797407558e5c89e6209dea5537b898
Joubert RedRat <eu+github@redrat.com.br>
2018-09-01 16:30:26 -0300

    Add new test

--- tests/AppProva/ScoreTest.php
+++ tests/AppProva/ScoreTest.php
@@ -5,6 +5,11 @@
 
 namespace Tests\AppProva;
 
+use AppProva\Domain\Entity\Course;
+use AppProva\Domain\Entity\Institution;
+use AppProva\Domain\Service\CourseService;
+use AppProva\Domain\Service\InstitutionService;
+use AppProva\Domain\Service\ScoreService;
 use Tests\AppBundleTestCase;
 
 /**
@@ -15,18 +20,129 @@ use Tests\AppBundleTestCase;
 class ScoreTest extends AppBundleTestCase
 {
     /**
+     * @var Institution
+     */
+    protected $institution;
+
+    /**
+     * @var Course
+     */
+    protected $course;
+
+    /**
      * test AddScore
      *
      * @return void
+     * @throws \Throwable
      */
     public function testAddScore(): void
     {
+        $institution = $this->getInstitution();
+        $course = $this->getCourse();
         $institutionScore = 100;
         $courseGeneralScore = 100;
         $courseStudentAvgScore = 100;
 
-        self::assertSame($institutionScore, null);
-        self::assertSame($courseGeneralScore, null);
-        self::assertSame($courseStudentAvgScore, null);
+        $service = $this->getService();
+        $score = $service->scoreAdd(
+            $institution,
+            $course,
+            $institutionScore,
+            $courseGeneralScore,
+            $courseStudentAvgScore
+        );
+
+        self::assertSame($institutionScore, $score->getInstitutionScore());
+        self::assertSame($courseGeneralScore, $score->getCourseGeneralScore());
+        self::assertSame($courseStudentAvgScore, $score->getCourseStudentAvgScore());
+    }
+
+    /**
+     * test DeleteScore
+     *
+     * @return void
+     * @throws \Throwable
+     */
+    public function testDeleteScore(): void
+    {
+        $assert = false;
+
+        self::assertTrue($assert);
+    }
+
+    /**
+     * @return Institution
+     * @throws \Throwable
+     */
+    public function getInstitution(): Institution
+    {
+        if ($this->institution instanceof Institution) {
+            return $this->institution;
+        }
+
+
+        $repository = $this
+            ->getContainer()
+            ->get('app.repository.institution')
+        ;
+
+        $logger = $this
+            ->getContainer()
+            ->get('logger')
+        ;
+
+        $service = new InstitutionService($repository, $logger);
+
+        $name = "Faculdade A";
+        $this->institution = $service->institutionAdd($name);
+
+        return $this->institution;
+    }
+
+    /**
+     * @return Course
+     * @throws \Throwable
+     */
+    public function getCourse(): Course
+    {
+        if ($this->course instanceof Course) {
+            return $this->course;
+        }
+
+        $repository = $this
+            ->getContainer()
+            ->get('app.repository.course')
+        ;
+
+        $logger = $this
+            ->getContainer()
+            ->get('logger')
+        ;
+
+        $service = new CourseService($repository, $logger);
+
+        $name = "Sistemas de Informação";
+        $this->course = $service->courseAdd($name);
+
+        return $this->course;
+    }
+
+    /**
+     * @return ScoreService
+     * @throws \Exception
+     */
+    public function getService(): ScoreService
+    {
+        $repository = $this
+            ->getContainer()
+            ->get('app.repository.score')
+        ;
+
+        $logger = $this
+            ->getContainer()
+            ->get('logger')
+        ;
+
+        return new ScoreService($repository, $logger);
     }
 }
